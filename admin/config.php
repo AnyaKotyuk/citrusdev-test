5569cb119a3453ba46359b3d71cc256287cb1739
Anna <anya.kotyuk@gmail.com>
2018-03-04 12:56:09 +0200

    init commit

---
+++ admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc/admin/');
+define('HTTP_CATALOG', 'http://oc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc/admin/');
+define('HTTPS_CATALOG', 'http://oc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/oc/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/oc/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/oc/image/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/oc/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_SYSTEM . 'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc');
+define('DB_PASSWORD', 'oc');
+define('DB_DATABASE', 'oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
